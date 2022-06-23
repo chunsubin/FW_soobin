f552c3ac0a215c84674da186fa2f59a9c0829436
chunsubin <53314694+chunsubin@users.noreply.github.com>
2022-06-23 16:17:27 +0900

    Add files via upload

--- lab2/Counter4.v
+++ lab2/Counter4.v
@@ -4,13 +4,9 @@ module Counter4 (
 	input wire clr
 );
 
-initial begin
- Q = 4'b0000;
-end
-
 always @(posedge clk)
 begin
-	if (!clr | ( &Q ) ) Q = 4'b0000;  // if clear == 0 or Q == 4'b1111
+	if (!clr) Q = 4'b0000;  // if clear == 0 or Q == 4'b1111
 	else Q = Q + 1'b1;
 end
 
